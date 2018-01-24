5d371538b5e82b619666aece889c943ef936aeed
humyai3 <surawat_30449@hotmail.com>
2018-01-24 14:15:57 +0700

    max

---
+++ src/main/java/rmuti/Lab4/TestArrayListStack.java
@@ -0,0 +1,16 @@
+package rmuti.Lab4;
+
+public class TestArrayListStack {
+
+	public static void main(String[] args) {
+		ArrayListStack stack = new ArrayListStack();
+		stack.push("macos");
+		stack.push("linux");
+		stack.push("windows");
+		System.out.println(stack);
+		
+		String data = (String)stack.pop();
+		System.out.println(stack);
+	}
+
+	}
---
+++ src/main/java/rmuti/Lab4/TestQueue.java
@@ -0,0 +1,16 @@
+package rmuti.Lab4;
+
+public class TestQueue {
+
+	public static void main(String[] args) {
+		ArrayListQueue queue = new ArrayListQueue();
+		queue.enqueue("macos");
+		queue.enqueue("linux");
+		queue.enqueue("windows");
+		System.out.println(queue);
+		
+		String data = (String)queue.dequeue();
+	System.out.println(queue);	
+	}
+
+}
